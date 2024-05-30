2ea435f7d57b92e349a23d42afe4494d7ee3ddbd
Tassilo Winkelbauer <tassilo.winkelbauer@ffw.com>
2024-05-30 16:55:03 +0200

    Add navigation

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,34 +1,55 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import idle_game.composeapp.generated.resources.Res
-import idle_game.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        Screen()
+    }
+}
+
+@Composable
+@Preview
+fun Screen() {
+    Scaffold(
+        topBar = {
+            Text("Top bar")
+        },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+        }
+    )
+}
+
+@Composable
+fun Dingi(
+    name: String,
+    duration: String,
+    money: String,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier.fillMaxWidth()) {
+        Icon(Icons.Default.Call, contentDescription = null)
+        Column {
+            Text("$name")
+            Row {
+                Text("$duration min")
+                Text("$money Euro")
             }
         }
     }
